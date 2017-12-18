783a8ae9879c8abb7b6f22633b4a99ed16a5c47c
Eternal-Chaos <33375134+Eternal-Chaos@users.noreply.github.com>
2017-12-18 16:16:22 +0500

    Update EC.py

--- cogs/EC.py
+++ cogs/EC.py
@@ -7,35 +7,35 @@ class EC:
           self.bot = bot
 
 
-    @commands.group(invoke_without_command=True)
-    async def lenny(self, ctx):
-        """group commands"""
-        msg = 'Available: `{}lenny face`, `{}lenny shrug`, `{}lenny flip`, `{}unflip`, `{}lenny gib`'
-        await ctx.send(msg.format(ctx.prefix))
+     @commands.group(invoke_without_command=True)
+     async def lenny(self, ctx):
+         """group commands"""
+         msg = 'Available: `{}lenny face`, `{}lenny shrug`, `{}lenny flip`, `{}unflip`, `{}lenny gib`'
+         await ctx.send(msg.format(ctx.prefix))
 
-    @lenny.command()
-    async def shrug(self, ctx):
-        """Shrugs!"""
-        await ctx.message.edit(content='¯\\\_(ツ)\_/¯')
+     @lenny.command()
+     async def shrug(self, ctx):
+         """Shrugs!"""
+         await ctx.message.edit(content='¯\\\_(ツ)\_/¯')
 
-    @lenny.command()
-    async def tableflip(self, ctx):
-        """Tableflip!"""
-        await ctx.message.edit(content='(╯°□°）╯︵ ┻━┻')
+     @lenny.command()
+     async def tableflip(self, ctx):
+         """Tableflip!"""
+         await ctx.message.edit(content='(╯°□°）╯︵ ┻━┻')
 
-    @lenny.command()
-    async def unflip(self, ctx):
-        """Unflips!"""
-        await ctx.message.edit(content='┬─┬﻿ ノ( ゜-゜ノ)')
+     @lenny.command()
+     async def unflip(self, ctx):
+         """Unflips!"""
+         await ctx.message.edit(content='┬─┬﻿ ノ( ゜-゜ノ)')
 
-    @lenny.command()
-    async def lenny(self, ctx):
-        """Lenny Face!"""
-        await ctx.message.edit(content='( ͡° ͜ʖ ͡°)')
+     @lenny.command()
+     async def lenny(self, ctx):
+         """Lenny Face!"""
+         await ctx.message.edit(content='( ͡° ͜ʖ ͡°)')
     
-    @lenny.command()
-    async def gib(self, ctx):
-        await crx.message.edit(content='(づ｡◕‿‿◕｡)づ')
+     @lenny.command()
+     async def gib(self, ctx):
+         await crx.message.edit(content='(づ｡◕‿‿◕｡)づ')
     
 def setup(bot):
     bot.add_cog(EC(bot))
