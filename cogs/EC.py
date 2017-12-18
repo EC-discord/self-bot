6c1e2673c5932792128312df2b70368b51d25a76
Eternal-Chaos <33375134+Eternal-Chaos@users.noreply.github.com>
2017-12-18 16:27:12 +0500

    Update EC.py

--- cogs/EC.py
+++ cogs/EC.py
@@ -7,11 +7,11 @@ class EC:
           self.bot = bot
 
 
-     @commands.group(invoke_without_command=True)
-     async def lenny(self, ctx):
-         """group commands"""
-         msg = 'Available: `{}lenny face`, `{}lenny shrug`, `{}lenny flip`, `{}unflip`, `{}lenny gib`'
-         await ctx.send(msg.format(ctx.prefix))
+     #@commands.group(invoke_without_command=True)
+    # async def lenny(self, ctx):
+     #    """group commands"""
+     #    msg = 'Available: `{}lenny face`, `{}lenny shrug`, `{}lenny flip`, `{}unflip`, `{}lenny gib`'
+      #   await ctx.send(msg.format(ctx.prefix))
 
      @lenny.command()
      async def shrug(self, ctx):
