a8a1e3de39eacdc61a2e62f83ec7042b1098587a
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2017-12-23 19:00:19 +0500

    Update Ace.py

--- cogs/Ace.py
+++ cogs/Ace.py
@@ -6,13 +6,13 @@ class Ace:
      def __init__(self, bot):
           self.bot = bot
     
-@commands.command()
-async def rep(self,ctx):
-    await ctx.send("t!rep 394111231474270208")
+    @commands.command()
+    async def rep(self,ctx):
+        await ctx.send("t!rep 394111231474270208")
 
-@commands.command()
-async def daily(self,ctx):
-    await ctx.send("t!daily 394111231474270208")
+    @commands.command()
+    async def daily(self,ctx):
+        await ctx.send("t!daily 394111231474270208")
     
 def setup(bot):
     bot.add_cog(Ace(bot)
