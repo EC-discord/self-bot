d425899bf0abb3a72b0259e1ffbb2c45f3f65f8e
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2017-12-23 18:29:14 +0500

    Create Ace.py

---
+++ cogs/Ace.py
@@ -0,0 +1,20 @@
+import discord
+import asyncio
+
+class Ace: 
+     def __init__(self, bot):
+          self.bot = bot
+    
+@commands.command()
+async def r(self,ctx):
+    await ctx.message.send(content="t!rep 394111231474270208")
+
+@commands.command()
+async def r(self,ctx):
+    await ctx.message.send(content="t!daily 394111231474270208")
+    
+def setup(bot):
+    bot.add_cog(Ace(bot))
+
+    
+    
