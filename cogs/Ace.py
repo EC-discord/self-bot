87f2148dcd95b1bcda03c8d63c23a23146add002
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2017-12-23 20:47:50 +0500

    Update Ace.py

--- cogs/Ace.py
+++ cogs/Ace.py
@@ -15,12 +15,11 @@ class Ace:
          await ctx.send("t!daily <@264634027280039938>")
      
      @commands.command()
-     async def t(self,ctx):
-         self.bg_task = self.loop.create_task(self.t())
-         await self.wait_until_ready()
-         channel = self.get_channel(385439832362582027) # channel ID goes he
-         await channel.send("t!fish")
-         await asyncio.sleep(30)
+     async def t(self, ctx):
+         while True:
+             await asyncio.sleep(30)
+             channel = self.get_channel(385439832362582027)
+             await channel.send("t!fish")
     
 def setup(bot):
     bot.add_cog(Ace(bot))
