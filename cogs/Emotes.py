70533a161e8ec81724e0670e71800b529e43f368
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2017-12-24 11:29:24 +0500

    Update Emotes.py

--- cogs/Emotes.py
+++ cogs/Emotes.py
@@ -7,8 +7,16 @@ class Emotes:
           self.bot = bot
      
      @commands.command()
+     async def c(self, ctx):
+         await ctx.message.edit(content="<:GWmythicalGrandCat:371317556734197761>")  
+     
+     @commands.command()
      async def m(self, ctx):
-         await ctx.send("<:GWmythicalGrandCat:371317556734197761>")  
+         await ctx.message.edit(content="<:GWvictoriaMeguFace:371323891626541057>")
+     
+     @commands.command()
+     async def o(self, ctx):
+         await ctx.message.edit(content="<:GWmythicalThonkCool:367331557100224522>")
          
 def setup(bot):
     bot.add_cog(Emotes(bot))         
