0e4574e9e804abc5e15be40e6f32f7e9c3bc6020
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2018-01-09 17:02:32 +0500

    Update Anim.py

--- cogs/Anim.py
+++ cogs/Anim.py
@@ -11,9 +11,9 @@ class Anim:
          c=5
          for c in range(5, 0, -1):
             await ctx.message.edit(content="`THIS MESSAGE WILL SELF DESTRUCT IN %s`" % c)
-            await asyncio.sleep(0.9)
+            await asyncio.sleep(0.7)
          await ctx.message.edit(content="💣")
-         await asyncio.sleep(0.9)
+         await asyncio.sleep(0.7)
          await ctx.message.edit(content="💥")
        
 def setup(bot):
