d6526acefd24518c55ffde9eb67c15559453374d
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2018-01-09 20:51:56 +0500

    Update Anim.py

--- cogs/Anim.py
+++ cogs/Anim.py
@@ -18,7 +18,7 @@ class Anim:
      @commands.command
      async def hax(self, ctx):
          print ('be ye here the see dis command executesu')
-         await ctx.message.edit(content="`[▓▓▓                    ] / {virus}-virus.exe Packing files.`")
+         await ctx.send(content="`[▓▓▓                    ] / {virus}-virus.exe Packing files.`")
          await asyncio.sleep(0.4)
          await ctx.message.edit(content="`[▓▓▓▓▓▓▓                ] - {virus}-virus.exe Packing files..`")
          await asyncio.sleep(0.4)
