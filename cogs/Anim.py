fad56331b6905f8cfd1f3076b6cfb8bbba13dfc8
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2018-01-15 17:45:10 +0500

    Update Anim.py

--- cogs/Anim.py
+++ cogs/Anim.py
@@ -8,7 +8,6 @@ class Anim:
      
      
      async def on_message(self, message):
-         print ('woosh')
          if message.content.find('=bomb') != -1:
              await self._boom(message) 
           
