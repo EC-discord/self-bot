0cf1cb2f532557495762ffbc2a46fcefcdcd67cb
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2018-01-19 19:47:03 +0500

    Create skid.py

---
+++ cogs/skid.py
@@ -0,0 +1,19 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+class skid: 
+     def __init__(self, bot):
+          self.bot = bot
+     
+     @commands.command()
+     async def type(self, ctx):
+         while True:
+             await ctx.message.delete()
+             await channel.trigger_typing()
+         if message.content.startswith("=br"):
+             await ctx.message.delete()
+             break
+     
+def setup(bot):
+   bot.add_cog(skid(bot))     
