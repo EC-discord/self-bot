2b9203fddea3c8c2d18373be27cc5291a90b1a05
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2018-01-21 14:16:14 +0500

    Update skid.py

--- cogs/skid.py
+++ cogs/skid.py
@@ -6,6 +6,73 @@ class skid:
      def __init__(self, bot):
           self.bot = bot
      
+     @commands.command(pass_context=True, hidden=True, name='eval')
+     async def _eval(self, ctx, *, body: str):
+         """Evaluates python code"""
+
+         env = {
+             'bot': self.bot,
+             'ctx': ctx,
+             'channel': ctx.channel,
+             'author': ctx.author,
+             'guild': ctx.guild,
+             'message': ctx.message,
+             '_': self._last_result,
+             'source': inspect.getsource
+         }
+
+         env.update(globals())
+
+         body = self.cleanup_code(body)
+         #await self.edit_to_codeblock(ctx, body)
+         stdout = io.StringIO()
+         err = out = None
+
+         to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+         try:
+             exec(to_compile, env)
+         except Exception as e:
+             err = await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+             return await err.add_reaction('\u2049')
+
+         func = env['func']
+         try:
+             with redirect_stdout(stdout):
+                 ret = await func()
+         except Exception as e:
+             value = stdout.getvalue()
+             err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+         else:
+             value = stdout.getvalue()
+             if ret is None:
+                 if value:
+                     try:
+                         out = await ctx.send(f'```py\n{value}\n```')
+                     except:
+                         paginated_text = ctx.paginate(value)
+                         for page in paginated_text:
+                             if page == paginated_text[-1]:
+                                 out = await ctx.send(f'```py\n{page}\n```')
+                                 break
+                             await ctx.send(f'```py\n{page}\n```')
+             else:
+                 self._last_result = ret
+                 try:
+                     out = await ctx.send(f'```py\n{value}{ret}\n```')
+                 except:
+                     paginated_text = ctx.paginate(f"{value}{ret}")
+                     for page in paginated_text:
+                         if page == paginated_text[-1]:
+                             out = await ctx.send(f'```py\n{page}\n```')
+                             break
+                         await ctx.send(f'```py\n{page}\n```')
+
+         if out:
+             await out.add_reaction('\u2705') #tick
+         if err:
+             await err.add_reaction('\u2049') #x
+     
      @commands.command()
      async def type(self, ctx):
          while True:
