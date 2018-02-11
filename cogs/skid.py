5433456b66b57512fb3471ab77a1305016c7088d
EC <33375134+Eternal-Chaos@users.noreply.github.com>
2018-02-11 12:55:03 +0500

    Update skid.py

--- cogs/skid.py
+++ cogs/skid.py
@@ -178,7 +178,7 @@ class skid:
                 new_word = new_word + first_word + py
                 em = discord.Embed(color = 0xffd500)
                 em.description = ''.join(new_word)
-        await ctx.send(embed = em)
+            await ctx.send(embed = em)
         
             
         
