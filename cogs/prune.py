0c25bd7553238014b2dba8df50ae2bff01908ae1
EC <saeqaed@yahoo.com>
2018-08-12 15:07:55 +0500

    Update prune.py

--- cogs/prune.py
+++ cogs/prune.py
@@ -4,9 +4,10 @@ from discord.ext import commands
 class prune(object):
     def __init__(self, bot):
         self.bot = bot
-        
-    async def on_message(self, message):
-        if message.channel.id == 477902596397465603:
+        self.server = self.bot.get_guild(268597766652035072)
+        self.channel = self.bot.get_channel(477902596397465603)
+         
+    async def on_message(self, self.channel.message):
             await message.channel.prune()
             
 def setup(bot):
