0993f303d2308cb2d81a8224609180f7fe93ae20
EC <saeqaed@yahoo.com>
2019-02-16 21:47:24 +0500

    Update utils.py

--- cogs/utils.py
+++ cogs/utils.py
@@ -206,10 +206,10 @@ class Utility:
         if format is None and user.is_avatar_animated() != True:
             format = "png" 
         avatar = user.avatar_url_as(format = format if format is not "gif" else None, size = size)
-        async with ctx.session.get(user) as resp:
+        async with ctx.session.get(avatar) as resp:
             image = await resp.read()
         with io.BytesIO(image) as file:
-            await ctx.send(file = discord.File(user, f"image.{format}"))
+            await ctx.send(file = discord.File(avatar, f"image.{format}"))
 
 def setup(bot):
     bot.add_cog(Utility(bot))
