c487e19f41d60266d6d52cc91ca30a2a3e8663c8
EC <saeqaed@yahoo.com>
2019-03-15 16:02:06 +0500

    Update utils.py

--- cogs/utils.py
+++ cogs/utils.py
@@ -63,67 +63,6 @@ class Utility(commands.Cog):
         '''
         await ctx.send('`Selfbot Logging out`')
         await self.bot.logout()
-
-    @commands.command(name='help', hidden = 'True')
-    async def new_help_command(self, ctx, *commands : str):
-        """get help cmds"""
-        destination = ctx.message.author if self.bot.pm_help else ctx.message.channel
-
-        def repl(obj):
-            return self.bot._mentions_transforms.get(obj.group(0), '')
-
-        # help by itself just lists our own commands.
-        if len(commands) == 0:
-            pages = await self.bot.formatter.format_help_for(ctx, self.bot)
-        elif len(commands) == 1:
-            # try to see if it is a cog name
-            name = self.bot._mention_pattern.sub(repl, commands[0])
-            command = None
-            if name in self.bot.cogs:
-                command = self.bot.cogs[name]
-            else:
-                command = self.bot.all_commands.get(name)
-                if command is None:
-                    await destination.send(self.bot.command_not_found.format(name))
-                    return
-
-            pages = await self.bot.formatter.format_help_for(ctx, command)
-        else:
-            name = self.bot._mention_pattern.sub(repl, commands[0])
-            command = self.bot.all_commands.get(name)
-            if command is None:
-                await destination.send(self.bot.command_not_found.format(name))
-                return
-
-            for key in commands[1:]:
-                try:
-                    key = self.bot._mention_pattern.sub(repl, key)
-                    command = command.all_commands.get(key)
-                    if command is None:
-                        await destination.send(self.bot.command_not_found.format(key))
-                        return
-                except AttributeError:
-                    await destination.send(self.bot.command_has_no_subcommands.format(command, key))
-                    return
-
-            pages = await self.bot.formatter.format_help_for(ctx, command)
-
-        if self.bot.pm_help is None:
-            characters = sum(map(lambda l: len(l), pages))
-            # modify destination based on length of pages.
-            if characters > 1000:
-                destination = ctx.message.author
-
-        color = await ctx.get_dominant_color(ctx.author.avatar_url)
-
-        for embed in pages:
-            embed.color = color
-            try:
-                await ctx.send(embed=embed)
-            except discord.HTTPException:
-                em_list = await embedtobox.etb(embed)
-                for page in em_list:
-                    await ctx.send(page)
       
     @commands.command()
     async def nick(self, ctx, user : discord.Member, *, nickname : str = None):
