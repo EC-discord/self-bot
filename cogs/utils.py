0afc27ac34b0973bbe4e9379a176bc03eaa49f71
EC <saeqaed@yahoo.com>
2019-04-20 19:57:30 +0500

    Update utils.py

--- cogs/utils.py
+++ cogs/utils.py
@@ -135,7 +135,7 @@ class Utility(commands.Cog):
         __**Parameters**__
         • member – The tag, name or id of the user
         """
-        format = None
+        format = "gif"
         member = member or ctx.author
         if member.is_avatar_animated() != True:
 	        format = "png"
