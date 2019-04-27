5a3185971390bb4a728a132223175f904b5940c5
EC <saeqaed@yahoo.com>
2019-04-27 11:36:29 +0500

    Update helpformatter.py

--- ext/helpformatter.py
+++ ext/helpformatter.py
@@ -2,5 +2,5 @@ import discord
 from discord.ext.commands import DefaultHelpCommand
 
 class helpformatter(DefaultHelpCommand):  
-  def get_ending_note():
+  def get_ending_note(self):
     return "A bot made by Mirai#4437"
