8932586b6a2fb5769737ee4cdc2182a0702d4945
EC <saeqaed@yahoo.com>
2019-05-02 19:59:34 +0500

    Update Textemotes.py

--- cogs/Textemotes.py
+++ cogs/Textemotes.py
@@ -8,8 +8,11 @@ class Textemotes(commands.Cog):
           
      @commands.command()
      async def snipe(self, ctx, member : discord.Member = None):
-          member = member or " "
-          await ctx.message.edit(content = f'(　-_･) ︻デ═一"    ▸ {member.mention}')
+          member = member or None
+          if member is None:
+               await ctx.message.edit(content = '(　-_･) ︻デ═一"    ▸')
+          else:
+              await ctx.message.edit(content = f'(　-_･) ︻デ═一"    ▸ {member.mention}')
     
      @commands.command()
      async def powa(self, ctx):
