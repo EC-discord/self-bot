bedd2db357552293cd5a9114d2f0a8f8e0165037
EC <saeqaed@yahoo.com>
2019-05-25 10:46:14 +0500

    Update utils.py

--- cogs/utils.py
+++ cogs/utils.py
@@ -25,8 +25,8 @@ class Utility(commands.Cog):
         • text - the text to be translated
         """
         await ctx.send(translate(text, language))
-	await asyncio.sleep(2)
-	await ctx.message.delete()
+        await asyncio.sleep(2)
+        await ctx.message.delete()
         
     @commands.command()
     @commands.has_permissions(manage_emojis = True)
