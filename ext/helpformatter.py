7a55660ed5e1f7d488ef52c6b9ccffdb6a55cc79
EC <saeqaed@yahoo.com>
2019-09-07 18:00:25 +0500

    Update helpformatter.py

--- ext/helpformatter.py
+++ ext/helpformatter.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext.commands import DefaultHelpCommand
-from discord.ext.commands import Bot
 
 class helpformatter(DefaultHelpCommand):    
   def get_ending_note(self):
-    return f"A bot made by {bot.application_info().owner}"
+    return "A bot made by Mirai#6390"
