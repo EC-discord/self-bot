88714e53e7c9c43142bc8fe23ff8c79a205c14e6
EC <saeqaed@yahoo.com>
2019-09-30 20:37:53 +0500

    Update source.py

--- cogs/source.py
+++ cogs/source.py
@@ -7,10 +7,13 @@ class Source(commands.Cog):
         self.bot = bot
         
     @commands.command()
-    async def source(self, ctx, *, text: str):
-        """Shows source code of a command."""
+    async def source(self, ctx, *, command: str):
+        """shows source code of a command
+        Parameters
+        • command - the name of the command for which to display the source code
+        """
         nl = "```"
-        source_thing = inspect.getsource(self.bot.get_command(text).callback)
+        source_thing = inspect.getsource(self.bot.get_command(command).callback)
         await ctx.send(f"{nl}py\n{source_thing}{nl}")
         
 def setup(bot):
