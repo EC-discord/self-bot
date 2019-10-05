2dd4678b3199b7a5f4a52aa1feb740d2c0db9e58
EC <saeqaed@yahoo.com>
2019-10-05 11:32:36 +0500

    Update noble.py

--- cogs/noble.py
+++ cogs/noble.py
@@ -1,28 +1,21 @@
 import discord
-import operator
 import base64
 import asyncio
 import random
-import time
 import datetime
 import os
-import codecs
-import aiohttp
-import urllib.request
 import json
-import shutil
-import urllib.parse
 import glob
 import moviepy.editor as mpy
 from discord.ext import commands
-from PIL import Image,ImageFilter,ImageDraw,ImageFont
+from PIL import Image, ImageDraw, ImageFont
 from ext.colours import ColorNames
+import codecs
 
 
 class Noble(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.stopwatches = {}
 
     @commands.command()
     async def textgif(self,ctx,*,args):
@@ -44,29 +37,6 @@ class Noble(commands.Cog):
         await ctx.message.delete()
         for f in glob.glob("*.png"):
             os.remove(f)
-
-    @commands.command()
-    async def pil(self, ctx,args, *,member : discord.Member=None):
-        '''A SIMPLE DEMO FOR WELCOMING (DEV)'''
-        user = member or ctx.message.author
-        avi = user.avatar_url
-        url = avi
-        response = requests.get(url, stream=True)
-        with open('img.png', 'wb') as out_file:
-            shutil.copyfileobj(response.raw, out_file)
-        del response
-        img = Image.open("img.png")
-        img.thumbnail((200, 200))
-        new_im = Image.new("RGBA", (400,400))
-        img.thumbnail((150,150))
-        new_im.paste(img,(100,100))
-        font = ImageFont.truetype('./arial.ttf', 19)
-        xoff, yoff = (10,5)
-        d = ImageDraw.Draw(new_im)
-        d.text((90, 280), args, fill="green",font = font)
-        new_im.save("on_test.png")
-        await ctx.send(file=discord.File('on_test.png'))
-        await ctx.message.delete()
 
     @commands.command()
     async def pictext(self,ctx,*,args):
@@ -100,20 +70,6 @@ class Noble(commands.Cog):
         decoded_stuff = decoded_stuff[2:len(decoded_stuff)-1]
         await ctx.message.delete()
         await ctx.send(content = "{}".format(decoded_stuff))
-
-    @commands.command()
-    async def stopwatch(self, ctx):
-        """Starts/stops stopwatch"""
-        author = ctx.author
-        if not author.id in self.stopwatches:
-            self.stopwatches[author.id] = int(time.perf_counter())
-            await ctx.send(author.mention + " Stopwatch started!")
-        else:
-            tmp = abs(self.stopwatches[author.id] - int(time.perf_counter()))
-            tmp = str(datetime.timedelta(seconds=tmp))
-            await ctx.send(author.mention + " Stopwatch stopped! Time: **" + tmp + "**")
-            self.stopwatches.pop(author.id, None)
- 
 
 def setup(bot):
     bot.add_cog(Noble(bot))
