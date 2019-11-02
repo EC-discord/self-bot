2c9458febcdce8c2b4779fb465e0e7250ccc086a
EC <saeqaed@yahoo.com>
2019-11-02 11:18:48 +0500

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -107,7 +107,7 @@ class Selfbot(commands.Bot):
     async def on_message(self, message):
         r = re.compile(r">(#\d{6}) (.*)")
         r = r.match(message.content)
-        if r:
+        if r and (message.author == ctx.author):
             await message.delete()
             await message.channel.send(embed = discord.Embed(color = discord.Color(f"{r.group(1)}")), description = r.group(2))
         await self.process_commands(message)
