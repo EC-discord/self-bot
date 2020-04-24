dd1f305c030077465dfb12b98fd9fab15a746986
EC <saeqaed@yahoo.com>
2020-04-24 08:03:28 +0500

    fixed addemoji adding an emoji twice

--- cogs/utils.py
+++ cogs/utils.py
@@ -44,19 +44,19 @@ class utility(commands.Cog):
         
     @commands.command()
     @commands.has_permissions(manage_emojis = True)
-    async def addemoji(self, ctx, emoji_name, emoji_link = '', *roles: discord.Role):
+    async def addemoji(self, ctx, emoji_name, emoji_url = None, *roles: discord.Role):
         """add an emoji to a server
         Parameters
         • emoji_name – The emoji name. Must be at least 2 characters
-        • emoji_link – The url or attachment of an image to turn into an emoji
+        • emoji_url – The url or attachment of an image to turn into an emoji
         • roles – A list of roles that can use this emoji. Leave empty to make it available to everyone
         """
         if ctx.message.attachments:
             emoji_link = ctx.message.attachments[0].url
             async with ctx.session.get(emoji_link) as resp:
                 image = await resp.read()
-        elif emoji_link:
-            async with ctx.session.get(emoji_link) as resp:
+        elif emoji_url:
+            async with ctx.session.get(emoji_url) as resp:
                 image = await resp.read()
         created_emoji = await ctx.guild.create_custom_emoji(name = emoji_name, image = image, roles = [r for r in roles if roles is not None])
         await ctx.send(f"Emoji {created_emoji.name} created!")
