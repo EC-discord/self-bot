443d2f1b5b6fef81ccaccdc326a747d89ac6be49
EC <saeqaed@yahoo.com>
2020-04-26 13:01:44 +0500

    Update utils.py

--- cogs/utils.py
+++ cogs/utils.py
@@ -44,12 +44,11 @@ class utility(commands.Cog):
         
     @commands.command()
     @commands.has_permissions(manage_emojis = True)
-    async def addemoji(self, ctx, emoji_name, emoji_url = None, *roles: discord.Role):
+    async def addemoji(self, ctx, emoji_name, emoji_url = None):
         """add an emoji to a server
         Parameters
         • emoji_name – The emoji name. Must be at least 2 characters
         • emoji_url – The url or attachment of an image to turn into an emoji
-        • roles – A list of roles that can use this emoji. Leave empty to make it available to everyone
         """
         if ctx.message.attachments:
             emoji_link = ctx.message.attachments[0].url
@@ -58,8 +57,9 @@ class utility(commands.Cog):
         elif emoji_url:
             async with ctx.session.get(emoji_url) as resp:
                 image = await resp.read()
-        created_emoji = await ctx.guild.create_custom_emoji(name = emoji_name, image = image, roles = [r for r in roles if roles is not None])
-        await ctx.send(f"Emoji {created_emoji.name} created!")
+        created_emoji = await ctx.guild.create_custom_emoji(name = emoji_name, image = image)
+        e = discord.utils.get(ctx.guild.emojis, name = emoji_name)
+        await ctx.send(f"Emoji {e.name} created!")
      
     @commands.command()
     async def delemoji(self, ctx, emoji: discord.Emoji):
