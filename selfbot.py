c2ff8813afe819184776da68d4fa00ba16d38c21
EC <saeqaed@yahoo.com>
2022-07-14 16:05:28 +0500

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -1,32 +1,30 @@
 import discord
-from discord import Color
 from discord.ext import commands
 from ext.context import CustomContext
 from ext.helpformatter import helpformatter
-from ext import embedtobox
-import aiohttp
 import json
 import os
 import re
 import traceback
 import sys
-    
+
+
 class Selfbot(commands.Bot):
     def __init__(self, **attrs):
-        super().__init__(command_prefix=self.get_pre, self_bot=True, help_command = helpformatter())
+        super().__init__(command_prefix=self.get_pre, self_bot=True, help_command=helpformatter())
         self.load_extensions()
         self.snipes = {}
-
+    
     def load_extensions(self):
-        for extension in ("anim", "misc", "mod", "noble", "skid", "source", "textemotes", "utils"):
+        for extension in os.listdir("./cogs"):
             try:
-                self.load_extension(f"cogs.{extension}")
-                print(f'Loaded extension: {extension}')
+                if extension.endswith(".py"):
+                    self.load_extension(f"cogs.{extension[:-3]}")
+                    print(f'Loaded extension: {extension}')
             except:
                 print(f'LoadError: {extension}\n'
                       f'{traceback.print_exc()}')
-
-   
+    
     @property
     def token(self):
         '''Returns your token wherever it is'''
@@ -38,17 +36,17 @@ class Selfbot(commands.Bot):
             else:
                 token = config.get('TOKEN').strip('\"')
         return os.environ.get('TOKEN') or token
-
+    
     @staticmethod
     async def get_pre(bot, message):
         '''Returns the prefix.'''
         with open('data/config.json') as f:
             prefix = json.load(f).get('PREFIX')
         return os.environ.get('PREFIX') or prefix or 'r.'
-
+    
     def restart(self):
         os.execv(sys.executable, ['python'] + sys.argv)
-
+    
     @staticmethod
     def run_wizard():
         '''Wizard for first start'''
@@ -57,16 +55,16 @@ class Selfbot(commands.Bot):
         print('------------------------------------------')
         prefix = input('Enter a prefix for your selfbot:\n> ')
         data = {
-                "TOKEN" : token,
-                "PREFIX" : prefix,
-            }
-        with open('data/config.json','w') as f:
+            "TOKEN":  token,
+            "PREFIX": prefix,
+        }
+        with open('data/config.json', 'w') as f:
             f.write(json.dumps(data, indent=4))
         print('------------------------------------------')
         print('Restarting...')
         print('------------------------------------------')
         os.execv(sys.executable, ['python'] + sys.argv)
-
+    
     @classmethod
     def init(bot, token=None):
         '''Starts the actual bot'''
@@ -76,15 +74,15 @@ class Selfbot(commands.Bot):
             selfbot.run(safe_token, bot=False, reconnect=True)
         except Exception as e:
             print(e)
-
+    
     async def on_connect(self):
         print('connected')
-
+    
     async def on_ready(self):
         '''Bot startup'''
         print('Logged in!')
         await self.change_presence(status=discord.Status.online, afk=True)
-
+    
     async def process_commands(self, message):
         '''Utilises the CustomContext subclass of discord.Context'''
         ctx = await self.get_context(message, cls=CustomContext)
@@ -92,27 +90,30 @@ class Selfbot(commands.Bot):
         if ctx.command is None:
             return
         await self.invoke(ctx)
-        
+    
     @classmethod
     def black(cls):
         return cls(0x000000)
-
-    discord.Color.black=black
-
+    
+    discord.Color.black = black
+    
     async def on_message_delete(self, message):
         if len(message.content) != 1:
             self.snipes[message.channel.id] = {"content": message.content, "author": message.author}
-        
+    
     async def on_message_edit(self, before, after):
         await self.process_commands(after)
-
+    
     async def on_message(self, message):
         r = re.compile(r">(#[0-9a-fA-F]{6}) (.*)")
         r = r.match(message.content)
-        if r and (self.user == message.author):           
+        if r and (self.user == message.author):
             await message.delete()
-            await message.channel.send(embed = discord.Embed(color = discord.Color(int("0x"+f"{r.group(1)[1:]}", 16)), description = r.group(2)))
+            await message.channel.send(
+                embed=discord.Embed(color=discord.Color(int("0x" + f"{r.group(1)[1:]}", 16)),
+                                    description=r.group(2)))
         await self.process_commands(message)
-                           
+
+
 if __name__ == '__main__':
     Selfbot.init()
